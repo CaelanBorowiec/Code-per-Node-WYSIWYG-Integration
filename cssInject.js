8de650d48280f21e83080ad5d4fa6046057bb43e
Caelan <caelan.borowiec@pqi.org>
2018-03-23 15:52:55 -0400

    Removed unused UrlExists.  Added comments.

--- cssInject.js
+++ cssInject.js
@@ -1,11 +1,4 @@
-function UrlExists(url)
-{
-    var http = new XMLHttpRequest();
-    http.open('HEAD', url, false);
-    http.send();
-    return http.status==200;
-}
-
+// Return the page NID using JS
 function getCurrentNodeId() {
   var $body = $('body.page-node');
   if ( ! $body.length )
@@ -19,11 +12,15 @@ function getCurrentNodeId() {
   return false;
 }
 
+
 function injectCPNCSS(){
+  //Check if CPN has saved a cached CSS file for this node.
+  // Note: this assumes the default storage location.
   $.get("/sites/default/files/cpn/"+getCurrentNodeId()+".css").done(function()
   {
-      $("iframe.cke_wysiwyg_frame").each(function()
+      $("iframe.cke_wysiwyg_frame").each(function()  // The 'cke_wysiwyg_frame' class can probably be replaced to support other editors
       {
+        // Attach the stylesheet link to the editor iframe.
         $head = $( this ).contents().find("head");
         $head.append('<link rel="stylesheet" href="/sites/default/files/cpn/'+getCurrentNodeId()+'.css" type="text/css" />');
       });
@@ -34,13 +31,13 @@ function injectCPNCSS(){
 };
 
 $(document).ready(function() {
-  setTimeout(function() {injectCPNCSS();}, 1000);
+  setTimeout(function() {injectCPNCSS();}, 1000);  // Wait 1 second after page load so that CKEditor can initialize.
 
   $( "a.ckeditor_links" ).click(function() {
-      setTimeout(function() {injectCPNCSS();}, 500);
+      setTimeout(function() {injectCPNCSS();}, 500); // Catches switching between 'rich text' and 'plain text'
   });
 
   $("select.form-select.ckeditor-processed").change(function(){
-    setTimeout(function() {injectCPNCSS();}, 700);
+    setTimeout(function() {injectCPNCSS();}, 700); // Catches switching between text formats (eg, from PHP to Full HTML)
   });
 });
